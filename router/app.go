83afc6d6c4efff6255ec20facd4014678c04c6e7
tiejiaxiaobao <2922035952@qq.com>
2024-01-18 23:54:41 +0800

    oj_sandbox infrastructure is completed

---
+++ router/app.go
@@ -0,0 +1,16 @@
+package router
+
+import (
+	"OJ_sandbox/service"
+	"github.com/gin-gonic/gin"
+)
+
+func Router() *gin.Engine {
+	r := gin.Default()
+
+	// 执行路由
+	r.GET("/test") // 后期可能用于集群判断
+	r.POST("/code_execute", service.CodeExecute)
+
+	return r
+}
---
+++ service/code_execute_service.go
@@ -0,0 +1,25 @@
+package service
+
+import "github.com/gin-gonic/gin"
+
+func CodeExecute(c *gin.Context) {
+	// 判断请求是否具有风险
+
+}
+
+// RunCodeByDocker
+// 操作docker执行代码
+func RunCodeByDocker() {
+	// 获取 docker client
+
+	// 拉取镜像
+
+	// 创建容器
+
+	// 启动容器
+
+	// 删除容器
+
+	// 删除镜像
+
+}
